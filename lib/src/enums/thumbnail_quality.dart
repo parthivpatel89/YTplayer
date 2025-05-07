c7202307c8396fb60fb60e27178954f8de26d5e5
Parthiv Patel <parthiv89patel@gmail.com>
2025-05-07 13:08:13 +0530

    initial commit

---
+++ lib/src/enums/thumbnail_quality.dart
@@ -0,0 +1,17 @@
+/// Quality of YouTube video thumbnail.
+class ThumbnailQuality {
+  /// 120*90
+  static const String defaultQuality = 'default';
+
+  /// 320*180
+  static const String medium = 'mqdefault';
+
+  /// 480*360
+  static const String high = 'hqdefault';
+
+  /// 640*480
+  static const String standard = 'sddefault';
+
+  /// Unscaled thumbnail
+  static const String max = 'maxresdefault';
+}
---
+++ test/ytplayer_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:ytplayer/ytplayer.dart';
+
+void main() {
+  test('Youtube player', () {
+    const YTPlayer();
+  });
+}
